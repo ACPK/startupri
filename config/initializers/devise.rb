171239e1bc7fa9421fbc339904eb272a6f0aa201
ACPK <ak@calllred.com>
2014-10-14 17:59:46 -0400

    devise key fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'team@startupri.org'
+
+  # Secrets File
+  config.secret_key = ENV['secret_key_base'] unless Rails.env.development?
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
