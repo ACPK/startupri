4bc9d6ce9bc130b28dd74b8ba4484e4387222f41
ACPK <ak@calllred.com>
2014-10-18 01:01:41 -0400

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # SSL
+  config.middleware.use Rack::SslEnforcer
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
